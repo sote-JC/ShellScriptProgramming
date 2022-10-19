43831691b4a62fed2f8b5d6a13d08edf3b309822
sote-JC <usetogame123@naver.com>
2022-10-19 17:41:43 +0900

    src files updated

--- src/ex3-2.sh
+++ src/ex3-2.sh
@@ -3,11 +3,11 @@ result=0
 case "$2" in
   +)
    result=`expr $1 + $3` 
-   echo $result
+   echo $result;;
   -)
    result=`expr $1 - $3`
-   echo $result
+   echo $result;;
   *)
-   echo "입력 형태가 잘못되었습니다."
+   echo "입력 형태가 잘못되었습니다.";;
 esac
 exit 0
